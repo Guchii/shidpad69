0d77e3d5159f2eeb7b96aea2b400436c1cba8b10
Shivom <shvomsrivastava@gmail.com>
2022-05-05 16:13:52 +0530

    🎉 start project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Footer from './Footer.svelte';
+  import Pad from './lib/Pad.svelte';
+</script>
+<div class="wrapper">
+  <h1 class="text-4xl text-center p-3">ShidPad 69</h1>
+  <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-2">
+    {#each [1, 3, 5, 6, 7, 8] as pad, i}
+    <Pad num={i}/>
+    {/each}
+  </div>
+</div>
+
+<div class="wrapper"><Footer/></div>
---
+++ src/lib/Pad.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import {Howl, Howler} from 'howler';
+const sound = new Howl({
+    src: [
+        "bruh.mp3"
+    ]
+})
+export let num;
+</script>
+
+<div on:click={() => sound.play()} class="duration-200 h-48 bg-gradient-to-r from-purple-500 to-pink-500 rounded-md active:shadow-xl hover:shadow-xl hover:z-10 shadow-red-500 grid place-items-center uppercase cursor-pointer">
+    Pad {num+1}
+</div>
