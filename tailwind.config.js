d47fef26f790f4d87865178bdfe80892893ace32
Shivom <shvomsrivastava@gmail.com>
2022-05-06 16:15:11 +0530

    chore: font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
-      fontFamily: ["Creepster", "cursive"],
+      fontFamily: {
+        sans: ["Righteous", "cursive"],
+      },
     },
   },
   plugins: [],
