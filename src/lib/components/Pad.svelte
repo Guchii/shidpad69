cf762dfbc1c92d222121a2c9eec288a186f41258
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-05-06 11:06:34 +0000

    Prevent audio from playing over each other
    Fixes #1

--- src/lib/components/Pad.svelte
+++ src/lib/components/Pad.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
   import { Howl } from "howler";
-  import { globalVolume } from "../../stores/volume";
+  import { currentlyPlayingAudio } from "../../stores/audio";
 
-  export let pad;
+  export let pad: { id: number; loc: string; name: string; oneShot: boolean };
 
   const sound = new Howl({
     src: [pad.loc],
-    volume: $globalVolume,
+    volume: 1,
   });
-
-  $: sound.volume($globalVolume / 100);
 </script>
 
 <div
-  on:click={() => sound.play()}
-  class="duration-200 h-48 bg-gradient-to-r from-purple-500 to-pink-500 text-white font-black rounded-md active:shadow-xl hover:shadow-xl hover:z-10 shadow-red-500 grid place-items-center uppercase cursor-pointer"
+  on:click={() => {
+    if (!$currentlyPlayingAudio.includes(pad.id) || pad.oneShot) {
+      $currentlyPlayingAudio = [...$currentlyPlayingAudio, pad.id];
+      sound.play();
+      sound.on("end", () => {
+        $currentlyPlayingAudio = $currentlyPlayingAudio.filter(
+          (i) => i !== pad.id
+        );
+      });
+    }
+  }}
+  class="duration-200 h-48 bg-gradient-to-r from-purple-500 to-pink-500 text-white font-black rounded-md active:shadow-xl hover:shadow-xl hover:z-10 shadow-red-500 grid place-items-center uppercase cursor-pointer select-none"
 >
   {pad.name}
 </div>
