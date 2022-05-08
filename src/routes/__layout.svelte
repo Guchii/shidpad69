911c492eca0b207f6336035097867dfc01207735
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-05-08 09:26:24 +0000

    feat: add meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import Footer from '$lib/components/FooterComponent.svelte';
 	import Header from '$lib/components/HeaderComponent.svelte';
+	import { MetaTags } from 'svelte-meta-tags';
 	import '../app.css';
 </script>
+
+<MetaTags
+	title="ShidPad69"
+	description="Shidpad is a joke project that is supposed to be used for playing popular audio clips. It can be used to annoy someone while they are playing a video game or make a conversation funnier - the posibilities are endless."
+/>
+
+<svelte:head>
+	<title>ShidPad69</title>
+</svelte:head>
 
 <div class="flex flex-col min-h-screen bg-raisin-black">
 	<Header />
