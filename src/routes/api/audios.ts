b1f880a5ce1d214e283edb270c8c956f76724cc7
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-05-10 09:57:14 +0000

    fix: change omg to long running

--- src/routes/api/audios.ts
+++ src/routes/api/audios.ts
@@ -1,22 +1,20 @@
+import { capitalCase } from 'change-case';
 import { readdirSync } from 'node:fs';
 import { join } from 'node:path';
-import { capitalCase } from 'change-case';
 
 /** @type {import('./audios').RequestHandler} */
 export async function get() {
-	const oneShot = readdirSync(join('static', 'audio', 'one-shot')).map((e) => ({
-		name: e.replace('.mp3', ''),
-		loc: `/audio/one-shot/${e}`
-	}));
-	const longRunning = readdirSync(join('static', 'audio', 'long-running')).map((e) => ({
-		name: e.replace('.mp3', ''),
-		loc: `/audio/long-running/${e}`
-	}));
-	let audios = oneShot.map((e) => ({ ...e, name: capitalCase(e.name), oneShot: true }));
-	audios = [
-		...audios,
-		...longRunning.map((e) => ({ ...e, name: capitalCase(e.name), oneShot: false }))
-	];
-	audios = audios.map((e, index) => ({ id: index, ...e }));
+	const _getPropsFromName = (name: string, oneShot: boolean) => ({
+		name: capitalCase(name.replace('.mp3', '')),
+		loc: `/audio/${oneShot ? 'one-shot' : 'long-running'}/${name}`,
+		oneShot: oneShot
+	});
+	const oneShot = readdirSync(join('static', 'audio', 'one-shot')).map((n) =>
+		_getPropsFromName(n, true)
+	);
+	const longRunning = readdirSync(join('static', 'audio', 'long-running')).map((n) =>
+		_getPropsFromName(n, false)
+	);
+	const audios = [...oneShot, ...longRunning].map((e, index) => ({ id: index, ...e }));
 	return { body: { audios } };
 }
